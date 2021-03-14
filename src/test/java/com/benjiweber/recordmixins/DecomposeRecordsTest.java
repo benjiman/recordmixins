2d95fb9be08af9156ed333e96a0b0c7f103c60ea
benji <git@benjiweber.com>
2021-03-14 09:54:01 +0000

    Decomposing records without needing a supertype

--- src/test/java/com/benjiweber/recordmixins/DecomposeRecordsTest.java
+++ src/test/java/com/benjiweber/recordmixins/DecomposeRecordsTest.java
@@ -4,51 +4,196 @@ import org.junit.Test;
 import typeref.MethodFinder;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
-import static com.benjiweber.recordmixins.DeconstructRecordsTest.If.withFallback;
+import static com.benjiweber.recordmixins.DecomposeRecordsTest.If.withFallback;
 import static org.junit.Assert.assertEquals;
 
-public class DeconstructRecordsTest {
+public class DecomposeRecordsTest {
 
     record Nums(Integer first, Integer last) {}
+    record Colour(Integer r, Integer g, Integer b) {}
+    record Wrapper(String value) {}
     record Name(String first, String last) {}
+    record NameWithMiddle(String first, String middle, String last) {}
 
     @Test
     public void decompose_matching_types() {
-        
+        AtomicReference<String> result = new AtomicReference<>("Fail");
+        Name name = new Name("Benji", "Weber");
+
+        If.<String,String>instance(name, (first, last) -> {
+            result.set(first.toLowerCase() + last.toLowerCase());
+        });
+
+        assertEquals("benjiweber", result.get());
     }
 
     @Test
-    public void example() {
-
-        Name name = new Name("Benji", "Weber");
-        Nums nums = new Nums(4,3);
-
-        If.<String,String>instance(name, (first, last) -> {
-            System.out.println(first + " " + last);
-        });
+    public void decompose_mismatching_types() {
+        AtomicReference<String> result = new AtomicReference<>("Nothing Happened");
+        Nums nums = new Nums(5,6);
 
         If.<String,String>instance(nums, (first, last) -> {
-            System.out.println("Side effect");
-            System.out.println(first + " " + last);
+            result.set(first.toLowerCase() + last.toLowerCase());
         });
 
-        String resultMatch = withFallback("none").If.<String,String>instance(name, (first, last) ->
-                first.toLowerCase() + " " + last.toLowerCase()
-        );
-        assertEquals("benji weber", resultMatch);
-        String resultNoMatch = withFallback("none").If.<String,String>instance(nums, (first, last) ->
-                first.toLowerCase() + " " + last.toLowerCase()
-        );
-        assertEquals("none", resultNoMatch);
+        assertEquals("Nothing Happened", result.get());
     }
+
+    @Test
+    public void decompose_matching_types_to_value() {
+        Name name = new Name("Benji", "Weber");
+
+        String result = withFallback("Fail").If.<String,String>instance(name, (first, last) ->
+            first.toLowerCase() + last.toLowerCase()
+        );
+
+        assertEquals("benjiweber", result);
+    }
+
+    @Test
+    public void decompose_mismatching_types_to_value() {
+        Nums nums = new Nums(5,6);
+
+        String result = withFallback("Nothing").If.<String,String>instance(nums, (first, last) ->
+                first.toLowerCase() + last.toLowerCase()
+        );
+
+        assertEquals("Nothing", result);
+    }
+
+    @Test
+    public void decompose_insufficient_arity_to_value() {
+        Wrapper wrapper = new Wrapper("Benji");
+
+        String result = withFallback("Nothing").If.<String,String>instance(wrapper, (first, last) ->
+                first.toLowerCase() + last.toLowerCase()
+        );
+
+        assertEquals("Nothing", result);
+    }
+
+    @Test
+    public void decompose_surplus_arity_to_value() {
+        NameWithMiddle name = new NameWithMiddle("Benji", "???", "Weber");
+
+        String result = withFallback("Fail").If.<String,String>instance(name, (first, middle) ->
+                first.toLowerCase() + middle.toLowerCase()
+        );
+
+        assertEquals("benji???", result);
+    }
+
+    @Test
+    public void decompose_matching_types_triple() {
+        AtomicReference<Integer> result = new AtomicReference<>(-1);
+        Colour c = new Colour(5,6,7);
+
+        If.<Integer,Integer,Integer>instance(c, (r, g, b) -> {
+            result.set(r + g + b);
+        });
+
+        assertEquals(Integer.valueOf(18), result.get());
+    }
+
+    @Test
+    public void decompose_matching_types_to_value_triple() {
+        Colour c = new Colour(5,6,7);
+
+        int result = withFallback(-1).If.<Integer,Integer,Integer>instance(c, (r, g, b) ->
+            r + g + b
+        );
+
+        assertEquals(18, result);
+    }
+
+    @Test
+    public void decompose_mismatching_types_triple() {
+        AtomicReference<String> result = new AtomicReference<>("Nothing Happened");
+        Colour c = new Colour(5,6,7);
+
+        If.<String,String,String>instance(c, (r, g, b) -> {
+            result.set(r.toLowerCase() + r.toLowerCase());
+        });
+
+        assertEquals("Nothing Happened", result.get());
+    }
+
+    @Test
+    public void decompose_mismatching_types_to_value_triple() {
+        Colour c = new Colour(5,6,7);
+
+        String result = withFallback("Expected").If.<Integer,String,Integer>instance(c, (r, g, b) ->
+                r + g + b
+        );
+
+        assertEquals("Expected", result);
+    }
+
+    @Test
+    public void decompose_supertypes() {
+        interface Animal { String noise(); }
+        record Duck(String noise) implements Animal {}
+        record Dog(String noise) implements Animal {}
+
+        record Zoo(Animal one, Animal two) {}
+
+        Zoo zoo = new Zoo(new Duck("Quack"), new Dog("Woof"));
+
+        String result = withFallback("Fail").If.<Animal,Animal>instance(zoo, (duck, dog) ->
+                duck.noise() + dog.noise()
+        );
+
+        assertEquals("QuackWoof", result);
+    }
+
+    @Test
+    public void decompose_subtypes() {
+        interface Animal { String noise(); }
+        record Duck(String noise) implements Animal {}
+        record Dog(String noise) implements Animal {}
+
+        record Zoo(Animal one, Animal two) {}
+
+        Zoo zoo = new Zoo(new Duck("Quack"), new Dog("Woof"));
+
+        String result = withFallback("Fail").If.<Duck,Dog>instance(zoo, (duck, dog) ->
+                duck.noise() + dog.noise()
+        );
+
+        assertEquals("QuackWoof", result);
+    }
+
+    @Test
+    public void decompose_mismatching_subtypes() {
+        interface Animal { String noise(); }
+        record Duck(String noise) implements Animal {}
+        record Dog(String noise) implements Animal {}
+
+        record Zoo(Animal one, Animal two) {}
+
+        Zoo zoo = new Zoo(new Duck("Quack"), new Dog("Woof"));
+
+        String result = withFallback("Fail").If.<Dog,Duck>instance(zoo, (duck, dog) ->
+                duck.noise() + dog.noise()
+        );
+
+        assertEquals("Fail", result);
+    }
+
 
     interface ParamTypeAware extends MethodFinder {
         default Class<?> paramType(int n) {
-            return method().getParameters()[n].getType();
+            return method().getParameters()[(actualParamCount() - expectedParamCount()) + n].getType();
         }
+        int expectedParamCount();
+        private int actualParamCount() {
+            return method().getParameters().length;
+        }
+
     }
     interface MethodAwareBiFunction<L,R,TResult> extends BiFunction<L,R,TResult>, ParamTypeAware {
         default Optional<TResult> tryApply(L left, R right) {
@@ -61,6 +206,7 @@ public class DeconstructRecordsTest {
             return paramType(0).isAssignableFrom(left.getClass())
                     && paramType(1).isAssignableFrom(right.getClass());
         }
+        default int expectedParamCount() { return 2; }
     }
 
     interface MethodAwareBiConsumer<L,R> extends BiConsumer<L,R>, ParamTypeAware {
@@ -74,6 +220,7 @@ public class DeconstructRecordsTest {
             return paramType(0).isAssignableFrom(left.getClass())
                     && paramType(1).isAssignableFrom(right.getClass());
         }
+        default int expectedParamCount() { return 2; }
     }
 
     interface TriFunction<T,U,V,R> {
@@ -91,6 +238,7 @@ public class DeconstructRecordsTest {
                     && paramType(1).isAssignableFrom(two.getClass())
                     && paramType(2).isAssignableFrom(three.getClass());
         }
+        default int expectedParamCount() { return 3; }
     }
 
     interface TriConsumer<T,U,V> {
@@ -108,6 +256,7 @@ public class DeconstructRecordsTest {
                     && paramType(1).isAssignableFrom(two.getClass())
                     && paramType(2).isAssignableFrom(three.getClass());
         }
+        default int expectedParamCount() { return 3; }
     }
     abstract static class Match<TResult> {
         public final Match<TResult> If = this;
@@ -118,11 +267,11 @@ public class DeconstructRecordsTest {
         static <TResult> Match<TResult> withFallback(TResult defaultResult) {
             return new Match<>() {
                 public <L, R> TResult instance(Object toMatch, MethodAwareBiFunction<L, R, TResult> action) {
-                    return DeconstructRecordsTest.If.instance(toMatch, action).orElse(defaultResult);
+                    return DecomposeRecordsTest.If.instance(toMatch, action).orElse(defaultResult);
                 }
 
                 public <T, U, V> TResult instance(Object toMatch, MethodAwareTriFunction<T, U, V, TResult> action) {
-                    return DeconstructRecordsTest.If.instance(toMatch, action).orElse(defaultResult);
+                    return DecomposeRecordsTest.If.instance(toMatch, action).orElse(defaultResult);
                 }
             };
         }
@@ -136,7 +285,7 @@ public class DeconstructRecordsTest {
         }
         static <T,U,V> void instance(Object o, MethodAwareTriConsumer<T,U,V> action) {
             if (o instanceof Record r) {
-                if (r.getClass().getRecordComponents().length < 2) {
+                if (r.getClass().getRecordComponents().length < 3) {
                     return;
                 }
                 action.tryAccept((T) nthComponent(0, r), (U) nthComponent(1, r), (V) nthComponent(2, r));
@@ -153,7 +302,7 @@ public class DeconstructRecordsTest {
         }
         static <T,U,V,TResult> Optional<TResult> instance(Object o, MethodAwareTriFunction<T,U,V,TResult> action) {
             if (o instanceof Record r) {
-                if (r.getClass().getRecordComponents().length < 2) {
+                if (r.getClass().getRecordComponents().length < 3) {
                     return Optional.empty();
                 }
                 return action.tryApply((T) nthComponent(0, r), (U) nthComponent(1, r), (V) nthComponent(2, r));
--- src/test/java/com/benjiweber/recordmixins/OptionalPatternMatchTest.java
+++ src/test/java/com/benjiweber/recordmixins/OptionalPatternMatchTest.java
@@ -1,2 +1,207 @@
-package com.benjiweber.recordmixins;public class OptionalPatternMatchTest {
+package com.benjiweber.recordmixins;
+
+import org.junit.Test;
+import typeref.MethodFinder;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+import static org.junit.Assert.assertEquals;
+import static com.benjiweber.recordmixins.OptionalPatternMatchTest.None.*;
+
+public class OptionalPatternMatchTest {
+
+    @Test
+    public void traditional_unwrap() {
+        Optional<String> unknown = Optional.of("Hello World");
+        assertEquals(
+                "hello world",
+                unknown
+                        .map(String::toLowerCase)
+                        .orElse("absent")
+        );
+    }
+
+    @Test
+    public void unwrap_optional() {
+        Optional<String> unknown = Optional.of("Hello World");
+        assertEquals(
+                "hello world",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_empty_optional() {
+        Optional<String> unknown = Optional.empty();
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_wrong_type_optional() {
+        Optional<Integer> unknown = Optional.of(5);
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_wrong_type_raw_optional() {
+        Optional unknown = Optional.of(5);
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_null() {
+        Optional<String> unknown = null;
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_unboxed_null() {
+        String unknown = null;
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_unboxed_value() {
+        String unknown = "Hello World";
+        assertEquals(
+                "hello world",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_unboxed_value_unknown_type() {
+        Object unknown = "Hello World";
+        assertEquals(
+                "hello world",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_unboxed_value_wrong_type() {
+        Integer unknown = 5;
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_unboxed_absent_unknown_type() {
+        Object unknown = null;
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_object_optional_empty() {
+        Object unknown = Optional.empty();
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_object_optional() {
+        Object unknown = Optional.of("Hello World");
+        assertEquals(
+                "hello world",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+
+    @Test
+    public void unwrap_raw_optional() {
+        Optional unknown = Optional.of("Hello World");
+        assertEquals(
+                "hello world",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_empty_raw_optional() {
+        Optional unknown = Optional.empty();
+        assertEquals(
+                "absent",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    @Test
+    public void unwrap_optionals_all_the_way_down() {
+        var unknown = Optional.of(Optional.of(Optional.of(Optional.of("Hello World"))));
+        assertEquals(
+                "hello world",
+                unwrap(unknown) instanceof String s
+                        ? s.toLowerCase()
+                        : "absent"
+        );
+    }
+
+    static Object unwrap(Object o) {
+        if (o instanceof Optional<?> opt) {
+            return opt.isPresent() ? unwrap(opt.get()) : None;
+        } else if (o != null) {
+            return o;
+        } else {
+            return None;
+        }
+    }
+    static class None {
+        private None() {}
+        public static final None None = new None();
+    }
+
+
 }
